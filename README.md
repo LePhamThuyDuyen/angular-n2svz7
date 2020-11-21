6d624f561cc9a0b6d2b4dce8384e2316d012703f
LePhamThuyDuyen <lephamthuyduyen2704@gmail.com>
2020-11-21 22:49:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-n2svz7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-n2svz7)
---
+++ src/app/form-field-overview-example.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/** @title Simple form field */
+@Component({
+  selector: 'form-field-overview-example',
+  templateUrl: 'form-field-overview-example.html',
+})
+export class FormFieldOverviewExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
